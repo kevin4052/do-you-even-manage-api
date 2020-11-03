a96b251647d14361a05b4409f3e05f709a0b64ae
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-11-03 17:37:15 -0500

    tested projects routes, need task binding

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -7,9 +7,8 @@ const Team = require('../models/Team.model');
 // ****************************************************************************************
 // POST - create a project
 // ****************************************************************************************
-router.post('/project', (req, res, next) => {
-  const { name, description, members, teamId } = req.body;
-  const user = req.user;
+router.post('/projects', (req, res, next) => {
+  const { name, description, teamId } = req.body;
 
   const newProject = {
     name,
@@ -21,7 +20,7 @@ router.post('/project', (req, res, next) => {
     .create(newProject)
     .then(projectDoc => {
 
-      console.log('projects', projectDoc)
+      console.log('projects', projectDoc);
 
       Team
         .findById(teamId)
@@ -32,62 +31,73 @@ router.post('/project', (req, res, next) => {
           teamDoc.projects.push(projectDoc._id);
           const updatedTeam = await teamDoc.save();
     
-          res.status(200).json({ project: projectDoc, user: updatedUser, team: updatedTeam });
+          res.status(200).json({ project: projectDoc, team: updatedTeam });
           
         })
         .catch(err => next(err));
     })
     .catch(err => next(err));
 
-});
-
-// ****************************************************************************************
-// GET route for all user related projects
-// ****************************************************************************************
-router.get('/user-projects', (req, res, next) => {
-  const user = req.user;
-  const { projects } = user;
-
-  Project.find({ _id: { $in: projects } })
-    .then(projectsDoc => res.status(200).json({ projects: projectsDoc }))
-    .catch(err => next(err));
-});
-  
+});  
 
 // ****************************************************************************************
 // GET route to get all the projects
 // ****************************************************************************************
-router.get('/project', (req, res, next) => {
-  Project.find()
-      .then(projectDoc => res.status(200).json({ project: projectDoc }))
-      .catch(err => next(err));
+router.get('/projects', (req, res, next) => {
+  Project
+    .find()
+    .then(projectDoc => res.status(200).json({ project: projectDoc }))
+    .catch(err => next(err));
 });
 
 // ****************************************************************************************
 // GET route for getting the project details
 // ****************************************************************************************
-router.get('/project/:projectId', (req, res) => {
-  Project.findById(req.params.projectId)
-      .then(foundProject => res.status(200).json({ project: foundProject }))
-      .catch(err => next(err));
+router.get('/projects/:projectId', (req, res) => {
+  Project
+    .findById(req.params.projectId)
+    .then(foundProject => res.status(200).json({ project: foundProject }))
+    .catch(err => next(err));
 });
 
 // ****************************************************************************************
 // POST route to save the updates
 // ****************************************************************************************
-router.post('/project/:projectId/update', (req, res) => {
-  Project.findByIdAndUpdate(req.params.projectId, req.body, { new: true })
-      .then(updatedProject => res.status(200).json({ project: updatedProject }))
-      .catch(err => next(err));
+router.post('/projects/:projectId/update', (req, res) => {
+  const { projectId } = req.params;
+  // const { name, description, tasks } = req.body;
+
+  Project
+    .findByIdAndUpdate(projectId, req.body, { new: true })
+    .then(updatedProject => res.status(200).json({ project: updatedProject }))
+    .catch(err => next(err));
+
 });
 
 // ****************************************************************************************
 // POST route to delete the project
 // ****************************************************************************************
-router.post('/project/:projectId/delete', (req, res) => {
-  Project.findByIdAndRemove(req.params.projectId)
-      .then(() => res.json({ message: 'Successfully removed!' }))
-      .catch(err => next(err));
+router.post('/projects/:projectId/delete', (req, res, next) => {
+  const { projectId } = req.params;
+
+  Project
+    .findById(projectId)
+    .then(projectDoc => {
+      // console.log('project to delete', projectDoc);
+
+      Team
+        .findByIdAndUpdate(projectDoc.team, { $pull: { projects: projectDoc._id } }, { new: true })
+        .then(updatedTeam => {
+          // console.log('team after pulled project', updatedTeam);
+
+          Project.findByIdAndRemove(req.params.projectId)
+              .then(() => res.json({ message: 'Successfully removed!' }))
+              .catch(err => next(err));
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+
 });
 
 module.exports = router;
